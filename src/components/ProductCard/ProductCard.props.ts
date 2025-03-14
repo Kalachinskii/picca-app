644027958a7348274c6f1f8c7e26091ae8a876ed
kalachinskii_vl <kalachinskii_vl@mail.ru>
2025-03-14 23:29:23 +0300

    Реализована карточка пиццы

---
+++ src/components/ProductCard/ProductCard.props.ts
@@ -0,0 +1,9 @@
+// какой набор данных будет передаваться
+export interface ProductCardProps {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+    price: number;
+    rating: number;
+}
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Heading from "../../components/Heading/Heading";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import styles from "./Menu.module.css";
 
@@ -8,6 +9,16 @@ export function Menu() {
             <div className={styles.head}>
                 <Heading>Меню</Heading>
                 <Search placeholder="Введите блюдо или состав" />
+            </div>
+            <div>
+                <ProductCard
+                    id={1}
+                    title="Наслаждение"
+                    description="Салями, руккола, помидоры, оливки"
+                    price={320}
+                    rating={4.3}
+                    image="/img-card.png"
+                />
             </div>
         </>
     );
